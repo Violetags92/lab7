3804ccaed9a1de860c8b426e1af7086d28670c7c
Yixuan Xu <Yixuan Xu>
2015-10-29 23:34:03 +0100

    all done

--- lab7/R/methods.R
+++ lab7/R/methods.R
@@ -1,9 +1,9 @@
 predict.ridgereg1 <- function(x, newdata = "default"){
   if(identical(newdata, "default")){
-    return(x$y_hat)
+    return(x$yhat)
   }else{
     X <- as.matrix(cbind(1, newdata))
-    y_hat <- X %*% x$bhat
+    y_hat <- as.vector(X %*% x$bhat)
     return(y_hat)
   }
 }
