b5fffa9a1c3a41bbc692dc081125be4b797ab595
Yixuan Xu <Yixuan Xu>
2015-10-30 02:41:18 +0100

    orignal -> dest

--- lab7/R/delays.R
+++ lab7/R/delays.R
@@ -10,7 +10,7 @@ visualize_airport_delays <- function(){
   airports_select <- select(airports_filter, one_of(c("faa","lat","lon")))
   flights_select <- select(flights_filter, one_of(c("dest", "dep_delay", "arr_delay")))
   all_dest <- inner_join(flights_select, airports_select, by =c("dest" = "faa"))
-  a <- summarise(group_by(all_origin, dest), m=mean(dep_delay, na.rm = TRUE), lat=mean(lat), lon=mean(lon))
+  a <- summarise(group_by(all_dest, dest), m=mean(dep_delay, na.rm = TRUE), lat=mean(lat), lon=mean(lon))
   q <- ggplot(a, aes(x= lon, y =lat))+geom_point()+geom_text(aes(label=m),hjust=0, vjust=0)
   return(q)
 }
